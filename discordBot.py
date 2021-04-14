48e2bbffd886c5c3602660ebd8eac3232b992e22
jason6356 <70106185+jason6356@users.noreply.github.com>
2021-04-14 18:20:31 +0800

    Update discordBot.py

--- discordBot.py
+++ discordBot.py
@@ -23,4 +23,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('ODMwNDQ3OTAxOTA1OTExODI5.YHG04A._NzmnX4H7oISEjUBqflNBgjtYw8')
+client.run(os.getenv('TOKEN'))
