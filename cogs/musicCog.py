debe294d2d3f0626030d4d6ffc4d089fc2e33359
jason6356 <70106185+jason6356@users.noreply.github.com>
2021-08-20 12:41:11 +0800

    update the file type to another type

--- cogs/musicCog.py
+++ cogs/musicCog.py
@@ -155,11 +155,8 @@ class musicCog(commands.Cog):
             #connect to the voice channel
             channel = ctx.message.author.voice.channel
             voice = await channel.connect()
-            source = FFmpegPCMAudio('wenhan.mp3')
+            source = FFmpegPCMAudio('cogs_wenhan.wav')
             player = voice.play(source)
-
-
-
 
 
     @commands.command(name="loop", help="Loop the bot")
