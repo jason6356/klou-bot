7ac01f9ab10a5f00a8e03f69128db564a7f8e9a7
jason6356 <70106185+jason6356@users.noreply.github.com>
2021-08-20 12:51:32 +0800

    aaa

--- cogs/musicCog.py
+++ cogs/musicCog.py
@@ -155,7 +155,7 @@ class musicCog(commands.Cog):
             #connect to the voice channel
             channel = ctx.message.author.voice.channel
             voice = await channel.connect()
-            source = FFmpegPCMAudio('cogs\\cogs_wenhan.wav\\')
+            source = FFmpegPCMAudio('cogs\\cogs_wenhan.wav')
             player = voice.play(source)
 
     @commands.command(name="loop", help="Loop the bot")
